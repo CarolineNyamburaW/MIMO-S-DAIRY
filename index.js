bb50a2deee70221171aed733a66c751c7dd2fa99
CarolineNyamburaW <caroline.nyambura@student.moringaschool.com>
2021-05-05 14:56:25 +0300

    wrote a function for production in Shade B

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var shadeA = function (){
   console.log("Your production in Shade A 510 litres per day")
 }
 shadeA();
+var shadeB = function (){
+  console.log("Your production in Shade B 308 litres per day")
+}
+shadeB();
